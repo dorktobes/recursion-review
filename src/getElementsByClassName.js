9688d9c3f57a35e127b0e181104e8c2483a889d1
dorktobes <t.hoshman@gmail.com>
2017-10-09 11:46:23 -0700

    pass getElements specs

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -4,7 +4,25 @@
 // };
 
 // But instead we're going to implement it from scratch:
-var getElementsByClassName = function(className
-) {
+var getElementsByClassName = function(className, node = document.body) {
   // your code here
+  // input : class name quering for
+  // output: array DOM objects that match class name
+
+  var results = [];
+
+  if (node.classList && node.classList.contains(className)) {
+    results.push(node);
+  }
+  
+  var nodes = node.childNodes;
+  if (nodes) {
+    for (var i = 0; i < nodes.length; i++) {
+      results = results.concat(getElementsByClassName(className, nodes[i]));
+    }
+  }
+
+  console.log(results);
+  return results;
+
 };
